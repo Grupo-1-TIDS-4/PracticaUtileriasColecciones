64d944db1f26b5be021682d5226c3b44b20f52b9
unknown <7178497@clases.edu.sv>
2024-05-30 19:07:19 -0600

    se termino el ejercicio1

--- src/ContarPalabra.java
+++ src/ContarPalabra.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContarPalabra {
     public static void main(String[] args) throws Exception {
+        
+         
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una frase: ");
+        String frase = scanner.nextLine(); 
+
+        int cantidadPalabras = contarPalabras(frase); 
+
+      
+        System.out.println("La frase contiene " + cantidadPalabras + " palabra(s).");
+
+        scanner.close(); 
+
         
     }
 
